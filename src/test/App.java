f8410da1c3339d08a079004353894960bbdaec28
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2017-01-18 16:12:41 +0100

    Initial commit

---
+++ src/test/App.java
@@ -0,0 +1,49 @@
+package test;
+
+public class App {
+	
+	private static int MAX_ITERATIONS = 100;
+	private static ApparentPower MAX_APPARENT = new ApparentPower(100000);
+	
+	public static void main(String[] args) {
+		ActivePower meterP = new ActivePower(-110000);
+		
+		PowerFactor phi = new PowerFactor(0.9);
+		
+		getOptimum(meterP, phi, MAX_APPARENT);		
+
+	}
+	
+	private static Power getOptimum(ActivePower meterP, PowerFactor phi, ApparentPower sMax) {
+		ActivePower p = new ActivePower(meterP.value() * -1);
+		
+		/*
+		 * Find...
+		 */
+		for(int i=0; i<MAX_ITERATIONS; i++) {
+			System.out.println(">> iter " + i);
+			System.out.println("P: " + p);
+
+			// erste Randbedingung: sqrt(p²+q²)
+			ApparentPower s = PowerUtils.getApparent(p, phi);
+			double s_error = s.value() - sMax.value();
+			if(s_error <= 0) {
+				// Bedingung erfüllt
+				System.out.println("S=" + s + "; Erfüllt! Fehler: " + s_error);
+				if (s_error > -1) {
+					// ist konvergiert -> fertig
+					System.out.println("fertig");
+					break;
+				} else {
+					p.set(p.value() - s_error);
+				}
+				
+			} else { 
+				// nicht erfüllt
+				System.out.println("S=" + s + "; Nicht erfüllt! Fehler: " + s_error);
+				p.set(p.value() - s_error);
+			}
+		}
+		return new Power(p, new ReactivePower(0));
+	}
+}
---
+++ src/test/Power.java
@@ -0,0 +1,115 @@
+package test;
+
+class Power {
+	public ActivePower active;
+	public ReactivePower reactive;
+
+	public Power(ActivePower active, ReactivePower reactive) {
+		this.active = active;
+		this.reactive = reactive;
+	}
+}
+
+class PowerUtils {
+
+	/*
+	 * Active
+	 */
+
+	/*
+	 * Reactive
+	 */
+
+	public static ReactivePower getReactive(ActivePower p, ApparentPower s) {
+		double q = Math.sqrt(Math.pow(s.value(), 2) - Math.pow(p.value(), 2));
+		return new ReactivePower(q);
+	}
+
+	/*
+	 * Apparent
+	 */
+	public static ApparentPower getApparent(ActivePower p, PowerFactor phi) {
+		double s = p.value() / phi.value();
+		return new ApparentPower(Math.abs(s));
+	}
+
+	public static ApparentPower getApparent(ActivePower p, ReactivePower q) {
+		double s = Math.sqrt(Math.pow(p.value(), 2) + Math.pow(q.value(), 2));
+		return new ApparentPower(s);
+	}
+
+	/*
+	 * Factor
+	 */
+
+	public static PowerFactor getFactor(ActivePower p, ApparentPower s) {
+		double phi = p.value() / s.value();
+		return new PowerFactor(phi);
+	}
+
+}
+
+class AbstractPower {
+	private double value;
+
+	public AbstractPower(double value) {
+		this.value = value;
+	}
+
+	public double value() {
+		return value;
+	}
+	
+	public AbstractPower add(double value) {
+		this.value += value;
+		return this;
+	}
+	
+	public void set(double value) {
+		this.value = value;
+	}
+}
+
+class ActivePower extends AbstractPower {
+	public ActivePower(double value) {
+		super(value);
+	}
+
+	@Override
+	public String toString() {
+		return value() + " W";
+	}
+}
+
+class ReactivePower extends AbstractPower {
+	public ReactivePower(double value) {
+		super(value);
+	}
+
+	@Override
+	public String toString() {
+		return value() + " VA";
+	}
+}
+
+class ApparentPower extends AbstractPower {
+	public ApparentPower(double value) {
+		super(value);
+	}
+
+	@Override
+	public String toString() {
+		return value() + " var";
+	}
+}
+
+class PowerFactor extends AbstractPower {
+	public PowerFactor(double value) {
+		super(value);
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(value());
+	}
+}
